ec9246967390ff1a96f454dd7a5421e9b8023a9e
Santiago Ospina <santyoshd@gmail.com>
2025-10-29 16:56:24 -0500

    Agregando Proyecto Calculadora a GitHub

---
+++ Proyecto1/Proyecto1/Proyecto1/Proyecto 1.sql
@@ -0,0 +1,15 @@
+USE MASTER;
+GO
+
+CREATE DATABASE CalculadoraDB;
+GO
+
+USE CalculadoraDB;
+GO
+
+CREATE TABLE Calculos 
+(
+Id INT IDENTITY(1,1) PRIMARY KEY,
+Operacion NVARCHAR(50),
+Resultado DECIMAL(18,2),
+);
---
+++ Proyecto1/Proyecto1/Proyecto1/Proyecto1/FrmHistorial.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Proyecto1
+{
+    partial class FrmHistorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvHistorial = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvHistorial
+            // 
+            this.dgvHistorial.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvHistorial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistorial.GridColor = System.Drawing.SystemColors.Control;
+            this.dgvHistorial.Location = new System.Drawing.Point(12, 107);
+            this.dgvHistorial.Name = "dgvHistorial";
+            this.dgvHistorial.RowHeadersWidth = 51;
+            this.dgvHistorial.RowTemplate.Height = 24;
+            this.dgvHistorial.Size = new System.Drawing.Size(343, 328);
+            this.dgvHistorial.TabIndex = 0;
+            this.dgvHistorial.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHistorial_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 84);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Historial";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FrmHistorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(367, 447);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvHistorial);
+            this.Name = "FrmHistorial";
+            this.Text = "Historial";
+            this.Load += new System.EventHandler(this.FrmHistorial_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvHistorial;
+        private System.Windows.Forms.Label label1;
+    }
+}
